7028986b51544c8682c59aeabf526412ac8a3ae7
RedTechX <jeremy.fangyc@gmail.com>
2024-02-24 14:47:09 +0800

    x = y = z, z = 4, y = x - 1 + 1,  y = ?

--- main.py
+++ main.py
@@ -3,21 +3,24 @@
 Q1: 
 '''
 
-# TODO: Write your code here
+wrinkle_in_time = ['It', 'was', 'a', 'stormy', 'night', '.']
+print(wrinkle_in_time)
 
 ##################################################
 '''
 Q2:
 '''
 
-# TODO: Write your code here
-
+wrinkle_in_time.insert(3, 'and')
+wrinkle_in_time.insert(3, 'dark')
+print(wrinkle_in_time)
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
+wrinkle_in_time[1] = 'is'
 
 ##################################################
 '''
@@ -26,6 +29,14 @@ Q4:
 
 # TODO: Write your code here
 
+for i in range(len(wrinkle_in_time) - 1, -1, -1):
+    w = wrinkle_in_time[i]
+    
+    if 'a' in w:
+        wrinkle_in_time.remove(w)
+
+print(wrinkle_in_time)
+
 ##################################################
 '''
 Q5:
@@ -33,6 +44,12 @@ Q5:
 
 # TODO: Write your code here
 
+even = []
+
+for i in range(10):
+    even.append((i) * 2)
+
+print(even)
 ##################################################
 '''
 Q6:
